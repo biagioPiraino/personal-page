c20fd44bce210ee4ee0e69c113f5c3eb07bd4d33
Biagio Piraino <biagiopirainowork@gmail.com>
2023-03-16 09:51:20 +0000

    implemented animationfor the intro message's 1st line

---
+++ scripts/intro.js
@@ -0,0 +1,35 @@
+// Implement typing effect on the "hello-line" section
+const hello_text = "Hello";
+const hello_comma = ","
+const hello_name = "I'm Biagio"
+const hello_speed = 55;
+
+let text_counter = 0;
+function typeHello() {
+  if (text_counter < hello_text.length) {
+    document.getElementById("hello-text").innerHTML += hello_text.charAt(text_counter);
+    text_counter++;
+    setTimeout(typeHello, hello_speed);
+  }
+}
+
+let comma_counter = 0;
+function typeComma() {
+  if (comma_counter < hello_comma.length) {
+    document.getElementById("hello-comma").innerHTML += hello_comma.charAt(comma_counter);
+    comma_counter++;
+  }
+}
+
+let name_counter = 0;
+function typeName() {
+  if (name_counter < hello_name.length) {
+    document.getElementById("hello-name").innerHTML += hello_name.charAt(name_counter);
+    name_counter++;
+    setTimeout(typeName, hello_speed);
+  }
+}
+
+typeHello()
+setTimeout(typeComma, ((hello_speed * hello_text.length) + 50));
+setTimeout(typeName,  (hello_speed * (hello_text.length + hello_comma.length) + 50))
