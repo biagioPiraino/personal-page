bb18dfbd32f09ab0c1693d458520407612215e30
Biagio Piraino <biagiopirainowork@gmail.com>
2023-03-16 10:23:32 +0000

    implemented typing effect on intro role line

--- scripts/intro.js
+++ scripts/intro.js
@@ -1,7 +1,7 @@
 // Implement typing effect on the "hello-line" section
-const hello_text = "Hello";
+const hello_text  = "Hello";
 const hello_comma = ","
-const hello_name = "I'm Biagio"
+const hello_name  = "I'm Biagio"
 const hello_speed = 55;
 
 let text_counter = 0;
@@ -31,5 +31,32 @@ function typeName() {
 }
 
 typeHello()
-setTimeout(typeComma, ((hello_speed * hello_text.length) + 50));
+setTimeout(typeComma, ((hello_speed * hello_text.length) + 50)); 
 setTimeout(typeName,  (hello_speed * (hello_text.length + hello_comma.length) + 50))
+
+// Implement typing effect on the "role-line" section
+const role_text  = "a Full Stack Developer";
+const role_dot   = "."
+const role_speed = 55;
+
+let role_text_counter = 0;
+function typeRoleDescription() {
+  if (role_text_counter < role_text.length) {
+    document.getElementById("role-description").innerHTML += role_text.charAt(role_text_counter);
+    role_text_counter++;
+    setTimeout(typeRoleDescription, role_speed);
+  }
+}
+
+let role_dot_counter = 0;
+function typeBlinkingDot() {
+  if (role_dot_counter < role_dot.length) {
+    document.getElementById("blinking-dot").innerHTML += role_dot.charAt(role_dot_counter);
+    role_dot_counter++;
+  }
+}
+
+const role_line_timeout = (hello_speed * (hello_text.length + hello_comma.length + hello_name.length) + 50)
+setTimeout(typeRoleDescription, role_line_timeout)
+const role_dot_timeout = role_line_timeout + (role_speed * role_text.length)
+setTimeout(typeBlinkingDot, role_dot_timeout)
