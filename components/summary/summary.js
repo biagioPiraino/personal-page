662ece461255cdb8d9ac090900723cf54db1b7f9
Biagio Piraino <biagiopirainowork@gmail.com>
2025-02-24 21:14:39 +0000

    refactor - added summary section

---
+++ components/summary/summary.js
@@ -0,0 +1,21 @@
+class SummaryComponent extends HTMLElement {
+    constructor() {
+        super()
+        
+        fetch("/components/summary/summary.html")
+            .then(response => {
+                return response.text()
+            })
+            .then(html => {
+                const parser = new DOMParser()
+                const doc = parser.parseFromString(html, "text/html")
+                const template = doc.getElementById("header-template")
+                const shadow = this.attachShadow({ mode: "open" })
+                shadow.appendChild(template.content.cloneNode(true));
+            })
+    }
+
+    connectedCallback() { }
+}
+
+customElements.define("summary-component", SummaryComponent)
